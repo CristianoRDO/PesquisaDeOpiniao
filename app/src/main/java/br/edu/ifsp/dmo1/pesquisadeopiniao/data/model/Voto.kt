8291adb928b6465d06a3bb830dbd55ee4b9fc2d9
CristianoRDO <crisrooliveira.2004@gmail.com>
2025-01-23 09:28:15 -0300

    Implementação Inicial da Estrutura MVVM

---
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisadeopiniao/data/model/Voto.kt
@@ -0,0 +1,4 @@
+package br.edu.ifsp.dmo1.pesquisadeopiniao.data.model
+
+class Voto {
+}
---
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisadeopiniao/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package br.edu.ifsp.dmo1.pesquisadeopiniao.ui.main
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application): AndroidViewModel(application) {
+}
