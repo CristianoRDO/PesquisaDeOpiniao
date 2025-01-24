5f23c9571d8d6749b356db5b223bfc448d09b6be
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-01-24 19:35:50 -0300

    Adicionando novos layouts

--- app/src/main/java/br/edu/ifsp/dmo1/pesquisadeopiniao/ui/main/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisadeopiniao/ui/main/MainActivity.kt
@@ -1,11 +1,7 @@
 package br.edu.ifsp.dmo1.pesquisadeopiniao.ui.main
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import br.edu.ifsp.dmo1.pesquisadeopiniao.R
 import br.edu.ifsp.dmo1.pesquisadeopiniao.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
