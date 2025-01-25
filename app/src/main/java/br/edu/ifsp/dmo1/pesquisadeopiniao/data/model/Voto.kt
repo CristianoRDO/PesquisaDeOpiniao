104ff48eacbd559768d5c73bd94a1245252058d1
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-01-25 10:26:31 -0300

    Criação do método de criptografia para a entidade voto

--- app/src/main/java/br/edu/ifsp/dmo1/pesquisadeopiniao/data/model/Voto.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisadeopiniao/data/model/Voto.kt
@@ -1,5 +1,27 @@
 package br.edu.ifsp.dmo1.pesquisadeopiniao.data.model
 
 import br.edu.ifsp.dmo1.pesquisadeopiniao.utils.OpcaoVoto
+import java.security.MessageDigest
+import java.time.LocalDateTime
 
-data class Voto(val codigo: String, val opcao: OpcaoVoto)
+class Voto(prontuario: String, val opcao: OpcaoVoto, fromDB: Boolean) {
+
+    var codigoVoto: String
+        private set
+
+    init {
+        if (!fromDB) {
+            this.codigoVoto = criptografar(prontuario)
+        } else {
+            this.codigoVoto = prontuario
+        }
+    }
+
+    private fun criptografar(prontuario: String): String {
+        val text = prontuario + LocalDateTime.now().toString()
+        val bytes = text.toByteArray()
+        val md = MessageDigest.getInstance("SHA-256")
+        val digest = md.digest(bytes)
+        return digest.joinToString("") { "%02x".format(it) }
+    }
+}
