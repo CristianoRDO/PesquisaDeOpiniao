64c02a874bffc73883bd6e7c7719d7d667a645ff
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-01-25 17:52:02 -0300

    Criação do layout de exibição da apuração dos votos

---
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisadeopiniao/ui/result/ResultActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsp.dmo1.pesquisadeopiniao.ui.result
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.dmo1.pesquisadeopiniao.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
