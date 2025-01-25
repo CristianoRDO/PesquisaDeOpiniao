3a49683cc0124f1a4cbaf7a26193a7c96f04c552
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2025-01-25 12:50:58 -0300

    Criação do repository de Voto

--- app/src/main/java/br/edu/ifsp/dmo1/pesquisadeopiniao/data/repository/VotoRepository.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisadeopiniao/data/repository/VotoRepository.kt
@@ -1,4 +1,18 @@
 package br.edu.ifsp.dmo1.pesquisadeopiniao.data.repository
 
-class VotoRepository {
+import android.content.Context
+import br.edu.ifsp.dmo1.pesquisadeopiniao.data.database.DatabaseHelper
+import br.edu.ifsp.dmo1.pesquisadeopiniao.data.database.VotoDao
+import br.edu.ifsp.dmo1.pesquisadeopiniao.data.model.Voto
+
+class VotoRepository(context: Context) {
+
+    private val dbHelper = DatabaseHelper(context)
+    private val votoDao = VotoDao(dbHelper)
+
+    fun insert(voto: Voto) = votoDao.insert(voto)
+
+    fun getTotalVoteByOption() = votoDao.getTotalVoteByOption()
+
+    fun getByCode(code: String) = votoDao.getByCode(code)
 }
